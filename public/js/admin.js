4602cf05ce40fbad7eb0786b848cdb4c91bd6f74
Nikhil.Madhamshettiwar <nikhil.madhamshettiwar@synechron.com>
2019-05-20 01:57:03 +0530

    added admin page

---
+++ public/js/admin.js
@@ -0,0 +1,23 @@
+/* HTML document is loaded. DOM is ready.
+-------------------------------------------*/
+$(function () {
+    function loadContestants() {
+        var contetstatHtmlString = "";
+        let counter =1;
+        $.getJSON("/contestants/", function (contestants) {
+            contestants.forEach(element => {
+                contetstatHtmlString = `${contetstatHtmlString}<tr>
+                <th scope="row">${counter}</th>
+                <td>${element.ID}</td>
+                <td>${element.Name}</td>
+                <td>${element.Votes}</td>
+            </tr>`
+            counter++;
+            });
+            $("#table-head").append(contetstatHtmlString);
+        });
+        // console.log($("#participans-container"));
+    }
+
+    loadContestants();
+});
